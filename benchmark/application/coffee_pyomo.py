fe9e7d5f4d75113498234423bf8d1a3ea75a03e9
pphuc25 <phanphuc1100@gmail.com>
2024-01-05 17:03:31 +0700

    chorse: add status on optiguide, add m to have local varibale on coffee

--- benchmark/application/coffee_pyomo.py
+++ benchmark/application/coffee_pyomo.py
@@ -27,37 +27,49 @@ dark_coffee_needed_for_cafe = {'cafe1': 20, 'cafe2': 20, 'cafe3': 100}
 model = ConcreteModel()
 
 # Variables
-model.x = Var(shipping_cost_from_supplier_to_roastery.keys(), domain=Integers, bounds=(0, None))
-model.y_light = Var(shipping_cost_from_roastery_to_cafe.keys(), domain=Integers, bounds=(0, None))
-model.y_dark = Var(shipping_cost_from_roastery_to_cafe.keys(), domain=Integers, bounds=(0, None))
+model.x = Var(shipping_cost_from_supplier_to_roastery.keys(),
+              domain=Integers, bounds=(0, None))
+model.y_light = Var(shipping_cost_from_roastery_to_cafe.keys(),
+                    domain=Integers, bounds=(0, None))
+model.y_dark = Var(shipping_cost_from_roastery_to_cafe.keys(),
+                   domain=Integers, bounds=(0, None))
 
 # Objective
 model.obj = Objective(expr=sum(model.x[i] * shipping_cost_from_supplier_to_roastery[i]
                                for i in shipping_cost_from_supplier_to_roastery) +
-                            sum(roasting_cost_light[r] * model.y_light[r, c] +
-                                roasting_cost_dark[r] * model.y_dark[r, c]
-                                for r, c in shipping_cost_from_roastery_to_cafe) +
-                            sum((model.y_light[j] + model.y_dark[j]) * shipping_cost_from_roastery_to_cafe[j]
-                                for j in shipping_cost_from_roastery_to_cafe),
+                      sum(roasting_cost_light[r] * model.y_light[r, c] +
+                          roasting_cost_dark[r] * model.y_dark[r, c]
+                          for r, c in shipping_cost_from_roastery_to_cafe) +
+                      sum((model.y_light[j] + model.y_dark[j]) * shipping_cost_from_roastery_to_cafe[j]
+                          for j in shipping_cost_from_roastery_to_cafe),
                       sense=minimize)
 
 # Constraints
+
+
 def flow_constraint(model, r):
     return sum(model.x[i] for i in shipping_cost_from_supplier_to_roastery if i[1] == r) == \
-           sum(model.y_light[j] + model.y_dark[j] for j in shipping_cost_from_roastery_to_cafe if j[0] == r)
+        sum(model.y_light[j] + model.y_dark[j]
+            for j in shipping_cost_from_roastery_to_cafe if j[0] == r)
+
 
 def supply_constraint(model, s):
     return sum(model.x[i] for i in shipping_cost_from_supplier_to_roastery if i[0] == s) <= capacity_in_supplier[s]
 
+
 def light_demand_constraint(model, c):
     return sum(model.y_light[j] for j in shipping_cost_from_roastery_to_cafe if j[1] == c) >= light_coffee_needed_for_cafe[c]
+
 
 def dark_demand_constraint(model, c):
     return sum(model.y_dark[j] for j in shipping_cost_from_roastery_to_cafe if j[1] == c) >= dark_coffee_needed_for_cafe[c]
 
+
 # model.constraints = ConstraintList()
-roasteries = list(set(i[1] for i in shipping_cost_from_supplier_to_roastery.keys()))
-suppliers = list(set(i[0] for i in shipping_cost_from_supplier_to_roastery.keys()))
+roasteries = list(set(i[1]
+                  for i in shipping_cost_from_supplier_to_roastery.keys()))
+suppliers = list(set(i[0]
+                 for i in shipping_cost_from_supplier_to_roastery.keys()))
 cafes = list(set(i[1] for i in shipping_cost_from_roastery_to_cafe.keys()))
 
 model.FlowConstraint = Constraint(roasteries, rule=flow_constraint)
@@ -66,7 +78,8 @@ model.LightDemandConstraint = Constraint(cafes, rule=light_demand_constraint)
 model.DarkDemandConstraint = Constraint(cafes, rule=dark_demand_constraint)
 
 # Solve
-solver = SolverFactory('glpk')  # You can change the solver as per your requirement
-solver.solve(model)
+# You can change the solver as per your requirement
+solver = SolverFactory('glpk')
+m = solver.solve(model)
 
 print('Optimal cost:', model.obj())
